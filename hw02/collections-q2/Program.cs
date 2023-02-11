b0867b68fc6349548ab0f381f1ee825e572b8754
unknown <koioszey@gmail.com>
2023-02-11 23:40:50 +0300

    Main 5 | HW02 uploaded

---
+++ hw02/collections-q2/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace HomeWork
+{
+    using System;
+
+    public class Hw02_Q2
+    {
+        const int   SIZE = 20,
+                    COUNT = 3;
+        static double totalOfAverages = 0;
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("HW02 // QUESTION 2\n");
+            int[] arrOfNums = new int[SIZE];
+            for (int i = 0; i < SIZE; i++)
+            {
+                Console.Write("Enter a number: ");
+                if ((arrOfNums[i] = inputNumber()) == -1)
+                    --i;
+            }
+            Console.Write("\nNumbers: ");
+            print(arrOfNums);
+
+            Array.Sort(arrOfNums);
+            int[]   maxThreeNums = new int[COUNT],
+                    minThreeNums = new int[COUNT];
+            for (int i = 0; i < COUNT; i++)
+            {
+                minThreeNums[i] = arrOfNums[i];
+                maxThreeNums[i] = arrOfNums[SIZE - i - 1];
+            }
+            Console.Write("\nMax three of array: ");
+            print(maxThreeNums);
+            Console.WriteLine("Average of the group: " + string.Format("{0:#.##}", calculateAverage(maxThreeNums)) + "\n");
+            Console.Write("\nMin three of array: ");
+            print(minThreeNums);
+            Console.WriteLine("Average of the group: " + string.Format("{0:#.##}", calculateAverage(minThreeNums)) + "\n");
+            Console.WriteLine("Total average of two groups: " + totalOfAverages + "\n");
+            return;
+        }
+
+        public static int inputNumber()
+        {
+            string? inp = Console.ReadLine();
+            if (inp != null && checkValidity(ref inp))
+                return Int32.Parse(inp);
+            Console.WriteLine("Enter a valid input!");
+            return -1; //error
+        }
+
+        public static bool checkValidity(ref string inp)
+        {
+            for (int i = 0; i < inp.Length; i++)
+            {
+                if (i == 0 && inp[0] == 45)
+                    ++i;
+                if (inp[i] - 48 < 0 || inp[i] - 48 > 9)
+                    return false;
+            }
+            return true;
+        }
+        public static double calculateAverage(int[] arr)
+        {
+            int total = 0;
+            foreach (var x in arr)
+                total += (int)x;
+            double avg = (double)total/arr.Length;
+            totalOfAverages += avg;
+            return avg;
+        }
+
+        public static void print(int[] arr)
+        {
+            foreach (var x in arr)
+                Console.Write(x + " ");
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ hw02/collections-q3/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace HomeWork
+{
+    using System;
+
+    public class Hw02_Q3
+    {
+        public static void Main(string[] args)
+        {
+            string? inp = null;
+            while (!takeSentence(ref inp));
+            char[] vowels = new char[1];
+            findVowels(ref inp, ref vowels);
+            Console.Write("\nVowels in the string: ");
+            print(vowels);
+        }
+
+        public static bool takeSentence(ref string? inp)
+        {
+            Console.Write("Enter a sentence: ");
+            inp = Console.ReadLine();
+            if (inp == null || inp.CompareTo("") != 1)
+            {
+                Console.WriteLine("NULL INPUT!");
+                return false;
+            }
+            return true;
+        }
+
+        public static void findVowels(ref string inp, ref char[] arr)
+        {
+            char[] vowels = {'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü'};
+            int count = 0;
+            string temp = inp.ToLower();
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (vowels.Contains(temp[i]))
+                {
+                    arr[i-count] = temp[i];
+                    Array.Resize(ref arr, arr.Length+1);
+                }
+                else
+                    ++count;
+            }
+        }
+
+        public static void print<T>(T[] arr)
+        {
+            foreach (var x in arr)
+                Console.Write(x + " ");
+            Console.WriteLine();
+        }
+    }     
+}

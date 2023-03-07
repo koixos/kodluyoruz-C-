4ced709b8e67fcfe134cc5450c79ebe880a00159
unknown <koioszey@gmail.com>
2023-03-07 14:33:38 +0300

    main 11 | Draw Triangle updated

--- projects/draw-tri/Program.cs
+++ projects/draw-tri/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Projects
+{
+    using System;
+
+    public class DrawTriangle
+    {
+        public static void Main()
+        {
+            Console.Write("Enter the size of the triangle: ");
+            int size = Int32.Parse(takeInput());
+            draw(size);
+        }
+
+        public static void draw(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n+i; j++)
+                {
+                    if (j < n-i-1)
+                        Console.Write(" ");
+                    else
+                        Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static string takeInput()
+        {
+            string? inp = Console.ReadLine();
+            if (inp == null || inp == "")
+                return "null";
+            return inp;
+        }
+    }
+}
